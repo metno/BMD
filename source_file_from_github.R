cefcd2bc8a3041e04ce0f0694b2bddbde1840ac3
kajsamp <kajsamp@met.no>
2022-09-14 10:57:00 +0200

    Add files via upload

---
+++ source_file_from_github.R
@@ -0,0 +1,6 @@
+
+## You need to install and load devtool
+require(devtools)
+
+## Get the url to the script from Github by pressing "Raw"
+source_url("https://raw.githubusercontent.com/metno/BMD/master/multi_downscale.monsoon_tmean.bangladesh.R")

cba5ea7afb17b85ca7104f0551bacfaafc3ea8f1
kajsamp <kajsamp@met.no>
2022-09-16 15:45:03 +0200

    moved files and changed directory structure

--- Exercises/data_fiddling.R
+++ Exercises/data_fiddling.R
@@ -8,4 +8,14 @@ library(ggplot2)
 file.obs <- "Observed_data.xlsx"
 
 # READ OBSERVATION FILE
-obs <- read_excel(fileobs)
+obs <- read_excel(file.obs)
+
+colnames(obs) <- obs[2,]
+
+obs <- obs[-c(1,2)]
+
+class(obs$`Dry_Bulb_Temperature(deg. C)`)
+obs$`Dry_Bulb_Temperature(deg. C)` <- as.numeric(class(obs$`Dry_Bulb_Temperature(deg. C)`))
+class(obs$`Dry_Bulb_Temperature(deg. C)`)
+
+obs2 <- read_excel(file.obs, skip=1)
